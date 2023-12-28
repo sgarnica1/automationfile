1755f921d6a01cb3c73aa12b99e0ff66b5a4ec5a
sgarnica1 <sgarnica1902@gmail.com>
2023-12-27 21:06:31 -0600

    feat: validate that dir is not being moved

--- src/fileautomation/fileautomation.py
+++ src/fileautomation/fileautomation.py
@@ -3,18 +3,18 @@
 import os
 
 downloads_path = "C:/Users/sgarn/Downloads"
-file_terminations = ["png", "jpg", "mp4", "pdf", "zip", "exe" ]
+file_terminations = ["png", "jpg", "mp4", "pdf", "zip", "exe"]
 
 def move(file: str, term: str) -> None:
-    if file.endswith(term):
-        dir = f'{downloads_path}/{term}'
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-        os.rename(f'{downloads_path}/{file}', f'{dir}/{file}')
+    dir = f'{downloads_path}/{term}'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    os.rename(f'{downloads_path}/{file}', f'{dir}/{file}')
 
 
 def main():
     download_files = os.listdir(downloads_path)
     for file in download_files:
         for term in file_terminations:
-            move(file, term)
+            if file.endswith(term) and os.path.isfile(f'{downloads_path}/{file}'):
+                move(file, term)
