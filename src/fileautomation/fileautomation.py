ff4bee1a75fc7e5919cd9a90c8a2addd1bc9264f
sgarnica1 <sgarnica1902@gmail.com>
2023-12-27 20:36:46 -0600

    feat: move all files with termination to corresponding dir

--- src/fileautomation/fileautomation.py
+++ src/fileautomation/fileautomation.py
@@ -5,11 +5,15 @@ import os
 downloads_path = "C:/Users/sgarn/Downloads"
 exe_path = "C:/Users/sgarn/Downloads/exe"
 
+file_terminations = ["png", "jpg", "mp4", "pdf", "zip", "exe" ]
+
 def main():
     download_files = os.listdir(downloads_path)
-
+    
     for file in download_files:
-        if file.endswith(".exe"):
-            if not os.path.exists(exe_path):
-                os.makedirs(exe_path)
-            os.rename(f'{downloads_path}/{file}', f'{exe_path}/{file}')
+        for term in file_terminations:
+            if file.endswith(term):
+                dir = f'{downloads_path}/{term}'
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                os.rename(f'{downloads_path}/{file}', f'{dir}/{file}')
