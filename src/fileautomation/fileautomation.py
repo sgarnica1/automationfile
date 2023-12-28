43903380a5c69e8389d7750edbde5106e0162b2d
sgarnica1 <sgarnica1902@gmail.com>
2023-12-27 20:38:56 -0600

    refactor: add move function

--- src/fileautomation/fileautomation.py
+++ src/fileautomation/fileautomation.py
@@ -3,17 +3,18 @@
 import os
 
 downloads_path = "C:/Users/sgarn/Downloads"
-exe_path = "C:/Users/sgarn/Downloads/exe"
-
 file_terminations = ["png", "jpg", "mp4", "pdf", "zip", "exe" ]
+
+def move(file: str, term: str) -> None:
+    if file.endswith(term):
+        dir = f'{downloads_path}/{term}'
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        os.rename(f'{downloads_path}/{file}', f'{dir}/{file}')
+
 
 def main():
     download_files = os.listdir(downloads_path)
-    
     for file in download_files:
         for term in file_terminations:
-            if file.endswith(term):
-                dir = f'{downloads_path}/{term}'
-                if not os.path.exists(dir):
-                    os.makedirs(dir)
-                os.rename(f'{downloads_path}/{file}', f'{dir}/{file}')
+            move(file, term)
