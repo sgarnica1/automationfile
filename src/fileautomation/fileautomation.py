434ca1932a4ffd1f6c4ffe5c4416272052389aaf
sgarnica1 <sgarnica1902@gmail.com>
2023-12-27 20:27:48 -0600

    feat: read file and move .exe files to specific dir

--- src/fileautomation/fileautomation.py
+++ src/fileautomation/fileautomation.py
@@ -1,4 +1,15 @@
 # fileautomation/fileautomation.py
 
+import os
+
+downloads_path = "C:/Users/sgarn/Downloads"
+exe_path = "C:/Users/sgarn/Downloads/exe"
+
 def main():
-    print(__name__)
+    download_files = os.listdir(downloads_path)
+
+    for file in download_files:
+        if file.endswith(".exe"):
+            if not os.path.exists(exe_path):
+                os.makedirs(exe_path)
+            os.rename(f'{downloads_path}/{file}', f'{exe_path}/{file}')
